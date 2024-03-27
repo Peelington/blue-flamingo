293a2582c5b8157015f7aab9b6c81add675491b7
alton peel <altonpeel123@gmail.com>
2024-03-27 17:10:28 -0400

    testing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,23 +12,23 @@ export default function Home(props) {
   // const {productItems} = props
   
   
-  const storeItems = productItems.map(product =>
-    <StoreProducts
-      key={product._id}
-      item={product.item}
-      type={product.type}
-      imgurl={product.imgurl}
-      price={product.price}
-      material={product.material}
-      productId = {product._id}
-    />
-  )
+  // const storeItems = productItems.map(product =>
+  //   <StoreProducts
+  //     key={product._id}
+  //     item={product.item}
+  //     type={product.type}
+  //     imgurl={product.imgurl}
+  //     price={product.price}
+  //     material={product.material}
+  //     productId = {product._id}
+  //   />
+  // )
 
 
   return (
     <>
       
-      {storeItems}
+      {/* {storeItems} */}
     </>
   )
 }
